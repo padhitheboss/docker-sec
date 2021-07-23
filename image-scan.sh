0ae2c5987a166ae0b50c17cf9b611b35d67cecba
Subham Padhi <subham@HOME-PC.lan>
2021-07-24 01:00:09 +0530

    image scanner Added

---
+++ image-scan.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#installing Trivy
+# Here for Demonstration Purpose we are using Trivy
+#Any Paid or opensouce toolscan be used forthis purpose
+install_trivy(){
+    echo"Installing Trivy"
+    wget https://github.com/aquasecurity/trivy/releases/download/v0.19.1/trivy_0.19.1_Linux-64bit.deb
+    chmod +x trivy_0.19.1_Linux-64bit.deb
+    dpkg -i trivy_0.19.1_Linux-64bit.deb
+}
+
+scan_image(){
+    echo "Specify the image name you want to scan:"
+    echo name
+    trivy image $name
+}
+
+scan_config(){
+    echo"Enter Path of folder containing dockerfile or directory conatining IaC:"
+    read loc
+    trivy config $loc
+}
+main_menu(){
+    clear
+    echo " "
+    echo "Scanner For Images And Config Files"
+    echo " "
+    echo "Choose a option from below:"
+         echo 1. Scan a Image
+         echo 2. Scan a Config File
+         echo 3. Exit
+    echo " "
+    echo -n "Enter Option No.:"
+    read option
+    case $option in
+	1)scan_image
+    read -n 1 -p "<Enter> for main menu"
+        main_menu
+        ;;
+    2)scan_config
+    read -n 1 -p "<Enter> for main menu"
+        main_menu
+        ;;
+    5)
+		function goout () {
+			TIME=2
+			echo " "
+			echo Exiting Scanner ......
+			sleep $TIME
+			exit 0
+		}
+		goout
+	;;
+
+esac
+}
+main_menu
+
+if [grep -q "USER" trivy config $loc];then
+    echo "ADD the following Lines to Docker File"
+    echo "RUN groupadd -r app_user && useradd -r -g app_user app_user"
+    echo "RUN chsh -s /usr/sbin/nologin root"
+    echo "The Application in container should not run with root privileges"
